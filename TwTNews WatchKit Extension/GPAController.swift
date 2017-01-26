d6f6666abf4a1f6936e10a40a285f654adc758e8
Halcao <nyz1500@gmail.com>
2017-01-26 23:11:31 +0800

    TODO: GPA checking

---
+++ TwTNews WatchKit Extension/GPAController.swift
@@ -0,0 +1,31 @@
+//
+//  GPAController.swift
+//  TwTNews
+//
+//  Created by Halcao on 2017/1/26.
+//  Copyright © 2017年 Halcao. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class GPAController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
